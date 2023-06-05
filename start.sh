48d104e3b823a290d47b97c2a649021df84d124b
Techy-Z <109435638+Techy-Z@users.noreply.github.com>
2023-06-06 02:19:56 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,13 @@
-echo "CLONING $UPSTREAM_BRANCH of CUSTOM REPO FROM $UPSTREAM_REPO "
-git clone -b $UPSTREAM_BRANCH $UPSTREAM_REPO
-
+rm -r /RBot
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "COULD NOT FOUND UPSTREAM REPO... EXITING..."
+  exit
+else
+  echo "CLONING CUSTOM REPO FROM $UPSTREAM_REPO "
+  git clone -b $UPSTREAM_BRANCH $UPSTREAM_REPO /RBot
+fi
+cd /RBot
 if [ -n $CONFIG_ENV_URL ]
 then
   echo "CONFIG_ENV_URL DETECTED! DOWNLOADING NOW..."
@@ -9,8 +16,32 @@ else
   echo "CONFIG_ENV_URL NOT FOUND!!! YOU WILL HAVE TO ADD CONFIGS MANUALLY"
 fi
 
+if [ -z $STARTCMD ]
+then
+  echo "COULD NOT FOUND STARTCMD(bash/python3)... EXITING..."
+  exit
+fi
+
+if [ -z $STARTFILE ]
+then
+  echo "COULD NOT FOUND STARTFILE... EXITING..."
+  exit
+fi
+
 echo 'INSTALLING REQUIREMENTS...'
-pip3 install --disable-pip-version-check -q -U -r requirements.txt
+pip3 install -U -r requirements.txt
 
 echo "Starting Bot...."
-python3 -m bot
+
+if [ $STARTCMD = "bash" ]
+then
+  bash $STARTFILE
+elif [ $STARTCMD = "python3" ]
+then
+  python3 $STARTFILE
+else
+  echo "STARTCMD INVALID! ABORTING..."
+  echo "STARTCMD INVALID! ABORTING..."
+  echo "STARTCMD INVALID! ABORTING..."
+  exit
+fi
